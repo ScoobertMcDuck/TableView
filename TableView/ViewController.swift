d7f9c0926a90c85f535cc2b4f283efeedde740cb
ScoobertMcDuck <jamesvarnum@outlook.com>
2024-02-25 10:14:03 -0500

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Charlotte Varnum on 2/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
